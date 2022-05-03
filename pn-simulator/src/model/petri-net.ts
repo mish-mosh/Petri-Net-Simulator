3456f7518314358f000173a15a694b53a9c48fec
mish-mosh <shammout@uni-bremen.de>
2022-05-03 16:15:11 +0200

    Init petri net

---
+++ pn-simulator/src/model/petri-net.ts
@@ -0,0 +1,20 @@
+interface Place {
+    name: string;
+}
+
+interface Transition {
+    name: string;
+}
+
+interface FlowRelation<Place, Transition> {
+    place: Place;
+    transition: Transition;
+}
+
+interface PetriNet {
+    places: Set<string>;
+    transitions: Set<Transition>;
+}
+
+
+let es: FlowRelation<Place, Transition> = {place: {name: "eshi"}, transition: {name: "eshu"}}
