fff8709fcb5744f6e5bc38b780da300a539ff6af
Moayad Shammout <moayad.shammout@mailbox.org>
2024-10-18 13:43:37 +0200

    Deploy to gitlab pages

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,14 @@
 const { defineConfig } = require('@vue/cli-service')
+
+function publicPath() {
+  if (process.env.CI_PAGES_URL) {
+    return new URL(process.env.CI_PAGES_URL).pathname
+  } else {
+    return '/'
+  }
+}
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: publicPath(),
 })
